9b8625834feff93f1768f87dc432e20655f9d8ed
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-29 01:33:31 +0530

    API working city hardcoded

--- weather/views.py
+++ weather/views.py
@@ -5,6 +5,15 @@ from django.shortcuts import render
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ccc3992728385e710009902808f28b61'
     city = 'Vadodara'
-    r = requests.get(url.format(city))
-    print(r.text)
-    return render(request, 'weather/index.html')
+    r = requests.get(url.format(city)).json()
+    city_weather = {
+        'city': city,
+        'temperature': int(r['main']['temp']) - 273,
+        'description': r['weather'][0]['description'] ,
+        'max-temp': r['main']['temp_max'] ,
+        'min-temp': r['main']['temp_min'] ,
+        'icon': r['weather'][0]['icon'],
+        'humidity': r['main']['humidity'],
+    }
+    print(city_weather)
+    return render(request, 'weather/index.html', city_weather)
