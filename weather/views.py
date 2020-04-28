60d2efd0c74509c0c94db41f046ec0fbbaebb1dc
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-29 01:10:16 +0530

    API working well

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,10 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ccc3992728385e710009902808f28b61'
+    city = 'Vadodara'
+    r = requests.get(url.format(city))
+    print(r.text)
+    return render(request, 'weather/index.html')
